9a437000ae9dd4b5ce65113df1a6ca29b1c9c7f4
Gabriel Souza <gabrie.ssouza@gmail.com>
2021-09-14 19:06:28 -0300

    feat(bo-profile-system): remove dependencies

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,12 +1,8 @@
 import { IOClients } from '@vtex/api'
 
-import { BuyerOrgClient } from './buyerOrgClient'
 import { ProfileSystemClient } from './profileSystem'
 
 export class Clients extends IOClients {
-  public get buyerOrg() {
-    return this.getOrSet('buyerOrg', BuyerOrgClient)
-  }
   public get profileSystem() {
     return this.getOrSet('profileSystem', ProfileSystemClient)
   }
